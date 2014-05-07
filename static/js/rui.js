decff8c4e03a5abd6944d4c7c1695b01d8674f22
Brendan Long <self@brendanlong.com>
2014-05-07 10:15:19 -0600

    Style everything nicely.

--- static/js/rui.js
+++ static/js/rui.js
@@ -1,37 +1,59 @@
 (function() {
     "use strict";
     
+    var DEFAULT_ICON = "images/default-icon.png";
+
     var uis = {};
     
     function getRuis() {
         $.get("api/remote-uis", function(data) {
-            var list = $("#ruiList");
+            var list = $("#rui-list");
+            var newUis = {};
             for (var i = 0; i < data.length; ++i) {
                 var ui = data[i];
-                if (uis[ui.id]) {
-                    $("#" + ui.id).remove();
+                if (!ui.iconUrl) {
+                    ui.iconUrl = DEFAULT_ICON;
                 }
-                uis[ui.id] = ui;
-
-                var element = $("<li/>", {
-                    "class": "rui",
-                    id: ui.id
-                });
-                var link = $("<a/>", {
-                    "class": "rui-link",
-                    href: ui.url
-                });
-                if (ui.iconUrl) {
-                    $("<img/>", {
+                newUis[ui.id] = true;
+                if (uis[ui.id]) {
+                    var element = $("#rui-element-" + ui.id);
+                    var link = element.find(".rui-link");
+                    link.attr("href", ui.url);
+                    element.find(".rui-name").text(ui.name);
+                    element.find(".rui-icon").attr("src", ui.iconUrl);
+                } else {
+                    var element = $("<li/>", {
+                        "class": "rui",
+                        id: "rui-element-" + ui.id
+                    });
+                    var link = $("<a/>", {
+                        "class": "rui-link",
+                        href: ui.url
+                    });
+                    var icon = $("<img/>", {
                         "class": "rui-icon",
                         src: ui.iconUrl
-                    }).appendTo(link);
+                    });
+                    icon.error(function() {
+                        var icon = $(this);
+                        if (icon.attr("src") !== DEFAULT_ICON) {
+                            icon.attr("src", DEFAULT_ICON);
+                        }
+                    });
+                    icon.appendTo(link);
+                    $("<span/>", {
+                        "class": "rui-name"
+                    }).text(ui.name).appendTo(link);
+                    link.appendTo(element);
+                    element.appendTo(list);
                 }
-                $("<span/>", {
-                    "class": "rui-name"
-                }).text(ui.name).appendTo(link);
-                link.appendTo(element);
-                element.appendTo(list);
+                uis[ui.id] = ui;
+            }
+            for (var key in uis) {
+                if (!(key in newUis)) {
+                    $("#rui-element-" + uis[key].id).remove();
+                    delete uis[key];
+                }
             }
         });
     }
